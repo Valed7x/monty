ff1571a37df2eeb146ef0f9390dd49a6cfbd3e7e
Valed7x <barnoucis@gmail.com>
2023-09-21 17:49:51 -0700

    update

--- monty.h
+++ monty.h
@@ -27,9 +27,9 @@
 typedef struct stack_s
 
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+int n;
+struct stack_s *prev;
+struct stack_s *next;
 } stack_t;
 
 /**
@@ -47,8 +47,8 @@ typedef struct stack_s
 typedef struct instruction_s
 
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+char *opcode;
+void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 extern stack_t *head;
