dfe407205f497211bf2b5dd1c26ba58a5ff3c1ee
Blakstorm <riz.essaib@gmail.com>
2023-09-21 17:17:32 -0700

    fix

--- s_str.c
+++ s_str.c
@@ -2,7 +2,9 @@
 
 /**
  * print_char - Prints the Ascii value.
+ *
  * @stack: Pointer to a pointer pointing to top node of the stack.
+ *
  * @line_number: Interger representing the line number of of the opcode.
  */
 
@@ -21,7 +23,9 @@ void print_char(stack_t **stack, unsigned int line_number)
 
 /**
  * print_str - Prints a string.
+ *
  * @stack: Pointer to a pointer pointing to top node of the stack.
+ *
  * @ln: Interger representing the line number of of the opcode.
  */
 
@@ -50,7 +54,9 @@ void print_str(stack_t **stack, __attribute__((unused))unsigned int ln)
 
 /**
  * rotl - Rotates the first node of the stack to the bottom.
+ *
  * @stack: Pointer to a pointer pointing to top node of the stack.
+ *
  * @ln: Interger representing the line number of of the opcode.
  */
 
@@ -75,7 +81,9 @@ void rotl(stack_t **stack, __attribute__((unused))unsigned int ln)
 
 /**
  * rotr - Rotates the last node of the stack to the top.
+ *
  * @stack: Pointer to a pointer pointing to top node of the stack.
+ *
  * @ln: Interger representing the line number of of the opcode.
  */
 
